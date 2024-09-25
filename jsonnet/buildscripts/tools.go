30d72b506abba81cc0813a79d194a02d7648ed28
Tiago Castro <tiagolobocastro@gmail.com>
2024-09-25 09:47:48 +0100

    test: test
    Signed-off-by: Tiago Castro <tiagolobocastro@gmail.com>

---
+++ jsonnet/buildscripts/tools.go
@@ -0,0 +1,15 @@
+//+build tools
+
+// Package tools tracks dependencies for tools that used in the build process.
+// See https://github.com/golang/go/wiki/Modules
+package tools
+
+import (
+	_ "github.com/brancz/gojsontoyaml"
+	_ "github.com/campoy/embedmd"
+	_ "github.com/google/go-jsonnet/cmd/jsonnet"
+	_ "github.com/google/go-jsonnet/cmd/jsonnet-lint"
+	_ "github.com/google/go-jsonnet/cmd/jsonnetfmt"
+	_ "github.com/jsonnet-bundler/jsonnet-bundler/cmd/jb"
+	_ "github.com/yannh/kubeconform/cmd/kubeconform"
+)
